0d6ee56733d313ac3ab04c8bf499cf3d2f36f211
farraazzura <faraazzura611@gmail.com>
2024-08-31 05:04:51 +0700

    first commit

---
+++ button.py
@@ -0,0 +1,28 @@
+# Impor tkinter
+import tkinter as tk
+
+# Membuat objek utama untuk GUI
+button = tk.Tk()
+
+# Mengatur judul halaman web
+button.title("Fara Azzura Dasgupta - Latihan Button")
+
+# Membuat label
+label = tk.Label(button, text="Masukkan Nama:")
+label.pack()
+
+# Membuat entry (input text)
+entry = tk.Entry(button)
+entry.pack()
+
+# Fungsi yang akan dijalankan saat tombol ditekan
+def on_button_click():
+    nama = entry.get()
+    label.config(text=f"Nama Anda: {nama}")
+
+# Membuat tombol
+button = tk.Button(button, text="Klik", command=on_button_click)
+button.pack()
+
+# Menjalankan GUI
+button.mainloop()
